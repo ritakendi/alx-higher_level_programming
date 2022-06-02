1d333171067a08aa37f8c89e3a05b0c26fce3d67
ritakendi <ritabetty77@gmail.com>
2022-06-02 16:29:49 +0300

    changes

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,4 @@
 from add_0 import add
 a = 1
 b = 2
-add(a, b)
+print("{} + {} = {}".format(a,b,add(a, b)))
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,3 +1,11 @@
 def add(a, b):
-    print("{} + {} = {}".format(a,b,a+b))
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
     return (a + b)
