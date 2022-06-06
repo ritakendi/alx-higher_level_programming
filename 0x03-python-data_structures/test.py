a8ec738d49640b842340da984ff4a154694b5890
ritakendi <ritabetty77@gmail.com>
2022-06-06 22:05:49 +0300

    del

--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -34,7 +34,7 @@ max_integer = __import__('9-max_integer').max_integer
 
 my_list = [1, 90, 2, 13, 34, 5, -13, 3]
 max_value = max_integer(my_list)
-print("Max: {}".format(max_value))"""
+print("Max: {}".format(max_value))
 #!/usr/bin/python3
 divisible_by_2 = __import__('10-divisible_by_2').divisible_by_2
 
@@ -44,5 +44,14 @@ list_result = divisible_by_2(my_list)
 i = 0
 while i < len(list_result):
     print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-    i += 1
+    i += 1"""
+  #!/usr/bin/python3
+delete_at = __import__('11-delete_at').delete_at
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
+  
 
