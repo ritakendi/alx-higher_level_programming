7e5d4495a679b122b5ce532e473e899e8cbf2236
ritakendi <ritabetty77@gmail.com>
2022-06-06 21:29:05 +0300

    biggest integer

--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -20,12 +20,19 @@ new_tuple = add_tuple(tuple_a, tuple_b)
 print(new_tuple)
 
 print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ())) """
+print(add_tuple(tuple_a, ())) 
 
 #!/usr/bin/python3
 multiple_returns = __import__('8-multiple_returns').multiple_returns
 
 sentence = "At school, I learnt C!"
 length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+print("Length: {:d} - First character: {}".format(length, first))"""
+
+#!/usr/bin/python3
+max_integer = __import__('9-max_integer').max_integer
+
+my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
 
