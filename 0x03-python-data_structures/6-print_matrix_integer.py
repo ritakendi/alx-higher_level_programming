fb6ea584638c7ed27c217589b98890f8516b1b36
ritakendi <ritabetty77@gmail.com>
2022-06-06 18:23:59 +0300

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+def print_matrix_integer(matrix=[[]]):
+    for left in range(len(matrix)):
+        for right in range(len(matrix[left])):
+            if right != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[left][right]), end='')
+        print()
