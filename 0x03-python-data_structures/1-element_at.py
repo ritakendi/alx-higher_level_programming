7a701d776210ea57c682ceabd78c985b75aff699
ritakendi <ritabetty77@gmail.com>
2022-06-06 17:54:38 +0300

    retrieves an index from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-     if idx < 0:
+    if idx < 0:
         return(None)
 
-    if idx > my_list:
+    if idx > len(my_list) - 1:
         return(None)
 
-        return(my_list)
+    return(my_list[idx])
+
+my_list = [1,2,3,4,5]
+idx = 5
+print(element_at(my_list, idx))
