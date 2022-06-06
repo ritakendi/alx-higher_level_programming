0ff6cf63bca19aa3c19e8e128aa0b87497d5555a
ritakendi <ritabetty77@gmail.com>
2022-06-06 18:39:57 +0300

    tuples

--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+"""print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
 
 matrix = [
     [1, 2, 3],
@@ -9,4 +9,14 @@ matrix = [
 
 print_matrix_integer(matrix)
 print("--")
-print_matrix_integer()
+print_matrix_integer()"""
+#!/usr/bin/python3
+add_tuple = __import__('7-add_tuple').add_tuple
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
