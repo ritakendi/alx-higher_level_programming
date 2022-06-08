97190f715c57c87bdb70f27074ea15f13c9303b7
ritakendi <ritabetty77@gmail.com>
2022-06-08 21:28:29 +0300

    lambda

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if my_list is None or len(my_list) < 1:
-        return 0
-    w = 0
-    we = 0
-    for t in my_list:
-        w += t[1]
-        we += t[0] * t[1]
-    return we / w
+def square_matrix_map(matrix=[]):
+    return list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
