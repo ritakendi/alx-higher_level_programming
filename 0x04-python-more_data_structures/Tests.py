c8a5caae2b06fe4a76b7fd0e4465654d46fc85f9
ritakendi <ritabetty77@gmail.com>
2022-06-08 18:40:49 +0300

    replace

--- 0x04-python-more_data_structures/Tests.py
+++ 0x04-python-more_data_structures/Tests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+"""#!/usr/bin/python3
 square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 
 matrix = [
@@ -9,4 +9,13 @@ matrix = [
 
 new_matrix = square_matrix_simple(matrix)
 print(new_matrix)
-print(matrix)
+print(matrix)"""
+
+#!/usr/bin/python3
+search_replace = __import__('1-search_replace').search_replace
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
