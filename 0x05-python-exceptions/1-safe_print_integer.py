80d90fdc0a0399ead90526789ccadb216b7263e9
ritakendi <ritabetty77@gmail.com>
2022-06-21 00:29:05 +0300

    removed comments

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""value can be any type.
- Return True if value is an integer
-Return False otherwise"""
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
