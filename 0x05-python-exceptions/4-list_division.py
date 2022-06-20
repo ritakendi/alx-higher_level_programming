df17c584bb41766dd5473db6b3064ba6be23546e
ritakendi <ritabetty77@gmail.com>
2022-06-20 20:47:43 +0300

    [fix] rermoved imports

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-from cgitb import reset
-from decimal import DivisionByZero
-
-
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    
     for elements in range(list_length):
         try:
             result = my_list_1[elements] / my_list_2[elements]
@@ -20,4 +15,5 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             new_list.append(result)
+            
     return new_list
