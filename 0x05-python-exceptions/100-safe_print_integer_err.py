89a5a5cdbb98a2d2db17d8cbba9c052a33a89ba0
ritakendi <ritabetty77@gmail.com>
2022-06-21 01:03:15 +0300

    removed whitespace

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
     except Exception as i:
         sys.stderr.write("Exception: {}\n".format(i))
-        return False
+        return (False)
     else:
-        return True
+        return (True)
