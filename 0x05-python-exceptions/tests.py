857d47ba9d3719a0e7b025ba20571bdfadc548fa
ritakendi <ritabetty77@gmail.com>
2022-06-20 20:53:28 +0300

    [Feature] Exception raised

--- 0x05-python-exceptions/tests.py
+++ 0x05-python-exceptions/tests.py
@@ -45,16 +45,24 @@
 # print("{:d} / {:d} = {}".format(a, b, result))
 
 #!/usr/bin/python3
-list_division = __import__('4-list_division').list_division
+# list_division = __import__('4-list_division').list_division
 
-my_l_1 = [10, 8, 4]
-my_l_2 = [2, 4, 4]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+# my_l_1 = [10, 8, 4]
+# my_l_2 = [2, 4, 4]
+# result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+# print(result)
 
-print("--")
+# print("--")
 
-my_l_1 = [10, 8, 4, 4]
-my_l_2 = [2, 0, "H", 2, 7]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+# my_l_1 = [10, 8, 4, 4]
+# my_l_2 = [2, 0, "H", 2, 7]
+# result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+# print(result)
+
+#!/usr/bin/python3
+raise_exception = __import__('5-raise_exception').raise_exception
+
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception raised")
