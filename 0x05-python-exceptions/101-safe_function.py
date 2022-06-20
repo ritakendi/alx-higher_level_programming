4707d4db405cc99b66725512bc5b41f5246fbb65
ritakendi <ritabetty77@gmail.com>
2022-06-21 01:07:03 +0300

    removed comments

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
     import sys
-    """Returns the result of the function,
-    Otherwise,
-    returns None if something happens during the function
-    and prints in stderr the error precede by Exception:"""
     try:
         result = fct(*args)
 
