61fdf9ebbe89a47ab9197316f608b9fa3bc5fd0b
ritakendi <ritabetty77@gmail.com>
2022-06-27 16:00:50 +0300

    trial 2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    
     if type(a) in [int, float]:
         a = int(a)
     else:
-        raise TypeError('a must be an integer'):
+        raise TypeError('a must be an integer')
     if type(b) in [int,float]:
         b = int(b)
     else:
