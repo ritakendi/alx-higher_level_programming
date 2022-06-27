8702bc258be21c97424de0bbed50b7c60bd351a9
ritakendi <ritabetty77@gmail.com>
2022-06-28 00:52:09 +0300

    trial

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -54,7 +54,7 @@ class Rectangle():
 
     @property
     def height(self):
-        """Get or set the height of the rectangle."""
+        """Get set the height of the rectangle."""
         return self.__height
 
     @height.setter
