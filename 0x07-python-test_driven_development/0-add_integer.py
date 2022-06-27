8495dd6de21226f830713d53fe2e84eddfe78370
ritakendi <ritabetty77@gmail.com>
2022-06-27 15:45:32 +0300

    trial

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    
+    if type(a) in [int, float]:
+        a = int(a)
+    else:
+        raise TypeError('a must be an integer'):
+    if type(b) in [int,float]:
+        b = int(b)
+    else:
+        raise TypeError('b must be an integer')
+    result = a + b
+    return result
