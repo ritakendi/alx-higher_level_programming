4383781ce1f96fa2c437090a36f07eb8597cf962
ritakendi <ritabetty77@gmail.com>
2022-06-28 12:06:28 -0700

    trial

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
+"""Locked class called Lockedclass"""
+
+
 class LockedClass:
-    """Locked class called Lockedclass"""
+"""Al locked class thatprevents the user from dynamically creating new
+    intances attributes except if the attribute is called first_name
+"""
 
-
-    __slots__ = 'first_name'
-
-    """Al locked class thatprevents the user from dynamically creating new          intances attributes except if the attribute is called first_name
-    """
+    __slots__ = ["first_name"]
