766ab6ecf275c4aaccc838f37a254aa42b7f39b0
ritakendi <ritabetty77@gmail.com>
2022-07-05 22:59:22 +0300

    trial

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-"""
-Function that constructs the pascal triangle
-"""
+# 14-pascal_triangle.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a Pascal's Triangle function."""
 
 
 def pascal_triangle(n):
-    """appends pascals to a list"""
-    la = []
-    for i in range(n):
-        la.append(''.join(map(str, str(11**i))))
-    return (la)
+    """Represent Pascal's Triangle of size n.
+
+    Returns a list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
