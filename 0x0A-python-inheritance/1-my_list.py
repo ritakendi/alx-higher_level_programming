81b44ef40c54cdae5c1e487055b61b3d56cdae24
ritakendi <ritabetty77@gmail.com>
2022-07-06 17:19:57 +0300

    trial

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class MyList that inherits from list"""
+
+
+class MyList(list):
+    """public instance,prints the list
+        sorted(ascendingsort
+    """
+    def print_sorted(self):
+        print(sorted(self))
+
