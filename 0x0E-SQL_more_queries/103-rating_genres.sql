3635e2ad14466fc9b76af702ddb18a15e6808ff9
ritakendi <ritabetty77@gmail.com>
2022-08-10 13:09:20 -0700

    genres by rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,4 +1,4 @@
---lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
 
 -- Records are ordered by descending rating.
 
