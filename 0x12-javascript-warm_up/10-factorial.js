fd8b5bf518c23d5878502e17574814e1bd168e99
ritakendi <ritabetty77@gmail.com>
2022-09-06 13:23:44 +0300

    adition script

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-
+const num = parseInt(process.argv[2]);
 function factorial (num) {
-  if (num > 0) {
-    return num * factorial(num - 1);
+  if (isNaN(num) || num === 1) {
+    return 1;
+  } else {
+    return (num * factorial(num - 1));
   }
-  return 1;
 }
 
-console.log(factorial(Number(process.argv[2])));
+console.log(factorial(num));
