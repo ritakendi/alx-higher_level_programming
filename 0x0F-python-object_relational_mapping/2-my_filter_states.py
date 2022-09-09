6e14db1d07dd3995e3fbb85a02802838fb170ea3
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2022-09-09 19:28:53 +0300

    new file: my_filter_states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""takes in an argumenyt(user input)
+    and displays all values inthe states table
+    where name matches the user input.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # arguments.
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+
+    # creating an instance of the 'cursor class' used to execute
+    # 'SQL queries.
+    c = db.cursor()
+
+    """taking the cursor object and calling the execute function
+        with the query and a tuple containing the values to substitute
+    """
+    c.execute("SELECT * FROM states WHERE name LIKE '{}'\
+    ORDER BY id ASC".format(sys.argv[4]))
+
+    # returns a list of states inthe database.
+    result = c.fetchall()
+
+    # print states one by one
+    for row in result:
+        print(row)
+    c.close()
+    db.close()
