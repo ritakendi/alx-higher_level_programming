d458cd99d1b44cf2b8627df688dc87deb3dfef72
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2022-09-22 22:03:50 +0300

    Ooops

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-'''takes in a URL, sends a request to that URL,
-    and displays the size of the bodyof the response
-'''
+# bash
 curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
