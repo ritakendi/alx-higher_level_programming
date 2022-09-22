a1e3c9d741f1d269c89f8ef8a4a795310029b329
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2022-09-22 22:02:09 +0300

    correct

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-#takes in a URL, sends a request to that URL, and displays the size of the bodyof the response
-curl -s $1 --include | grep -i Content-Length | awk '{print $2}'
+'''takes in a URL, sends a request to that URL,
+    and displays the size of the bodyof the response
+'''
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
