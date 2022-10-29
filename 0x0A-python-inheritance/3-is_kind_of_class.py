e433c01e0bc822650bb543256b4da4fed00acb1b
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2022-10-29 23:46:21 +0300

    removed trailing whitespace

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""A function that checks whether an object is an instsnce 
+"""A function that checks whether an object is an instsnce
 of a class inherited from."""
 
 
 def is_kind_of_class(obj, a_class):
     """
-        Return: True if isintance 
+        Return: True if isintance
                 otherwise False
     """
     return (isinstance(obj, a_class))
